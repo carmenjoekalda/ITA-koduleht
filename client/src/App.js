5ac53dca14a836949caab08eb2a6c501bd91d136
77f315 <77f315@gmail.com>
2025-03-07 00:21:14 +0200

    :)

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,17 @@
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom"; 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Praktika from "./pages/Praktika";
 
 function App() {
-  const isGitHubPages = process.env.PUBLIC_URL.includes('github.io');  
-  console.log(isGitHubPages)
   return (
-    <BrowserRouter basename={isGitHubPages ? "/ITA-koduleht" : ""}>
+    <BrowserRouter basename="/ITA-koduleht">
       <Routes>
-        <Route index element={<HomePage />} />
+      <Route index element={<HomePage />} />
         <Route path="/praktika" element={<Praktika />} />
       </Routes>
     </BrowserRouter>
   );
 }
-
 export default App;

86c16b90ca910d6e0bf469768e46aca6e0282817
77f315 <77f315@gmail.com>
2025-03-06 21:23:55 +0200

    please, please work, i beg you, every fix I try is a new commit and the beauty of the commit history suffers

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,17 @@
-import './App.scss'
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import HomePage from './pages/Home';
-import Praktika from './pages/Praktika';
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import Praktika from "./pages/Praktika";
 
 function App() {
   return (
-      <BrowserRouter basename="/ITA-koduleht">
+    <BrowserRouter basename="/ITA-koduleht">
       <Routes>
-        <Route
-          path="/"
-          element={
-            <HomePage/>
-          }
-          />
-          <Route
-            path="/praktika"
-            element={
-              <Praktika/>
-            }
-          />
-        </Routes>
-        </BrowserRouter>
+        <Route index element={<HomePage />} />
+        <Route path="/praktika" element={<Praktika />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

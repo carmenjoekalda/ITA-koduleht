f6b41e17dcc250379fd15bd67ea3443ad634988d
77f315 <77f315@gmail.com>
2025-03-07 00:27:03 +0200

    hash

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Praktika from "./pages/Praktika";
 
 function App() {
   return (
-    <BrowserRouter basename="/ITA-koduleht">
+    <HashRouter basename="/ITA-koduleht">
       <Routes>
-      <Route index element={<HomePage />} />
-        <Route path="/praktika" element={<Praktika />} />
+        <Route index element={<HomePage />} />
+        <Route path="praktika" element={<Praktika />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
+
 export default App;

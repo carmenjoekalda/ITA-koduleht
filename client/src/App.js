dcf95f20ac254b56e6b0fdb24c92becbc7575cc8
77f315 <77f315@gmail.com>
2025-03-09 23:54:25 +0200

    erialad top

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { HashRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import Praktika from "./pages/Praktika";
-
+import Erialad from "./pages/Erialad";
 function App() {
   return (
     <HashRouter>
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/praktika" element={<Praktika />} />
+        <Route path="/erialad" element={<Erialad/>}/>
       </Routes>
     </HashRouter>
   );
---
+++ client/src/pages/Erialad.js
@@ -0,0 +1,73 @@
+import React from "react";
+import NavigationBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "./Erialad.css";
+import { Arrow } from "../assets/icons";
+function Erialad() {
+  return (
+    <div>
+      <NavigationBar />
+      <div className="custom-padding bg-light">
+        <h1 className="Erialatitle">Erialad</h1>
+
+        <div className="osakondrect">
+          <div style={{ width: "20vw" }}>
+            <h1 className="p-5">It-kool</h1>
+          </div>
+          <div className="d-flex pt-3" style={{ gap: "1rem" }}>
+            <div>
+              <p>Eriala nimetus |</p>
+              <p>Eriala Nimetus |</p>
+            </div>
+            <div>
+              <p>Eriala nimetus |</p>
+              <p>Eriala Nimetus |</p>
+            </div>
+            <div>
+              <p>Eriala nimetus |</p>
+              <p>Eriala Nimetus |</p>
+            </div>
+            <div>
+              <p>Eriala nimetus</p>
+              <p>Eriala Nimetus</p>
+            </div>
+          </div>
+        </div>
+
+        <div className="osakondrect">
+          <div style={{ width: "20vw" }}>
+            <h1 className="p-5">Ärikool</h1>
+          </div>
+          <div className="d-flex pt-3" style={{ gap: "1rem" }}>
+            <p>Bürooassistent | </p>
+            <p>Sekretär</p>
+            <p> Raamatupidaja |</p>
+            <p> Väikeettevõtja</p>
+          </div>
+        </div>
+        <div style={{ height: "184px" }} />
+
+        <div className="d-flex flex-row justify-content-between px-4">
+          <div className="filter d-flex align-items-center justify-content-between px-4">
+            Haridusnõue
+            <Arrow />
+        
+          </div>
+          <div className="filter d-flex align-items-center justify-content-between px-4">
+            Haridusnõue
+            <Arrow />
+          </div>
+          <div className="filter d-flex align-items-center justify-content-between px-4">
+            Haridusnõue
+            <Arrow />
+          </div>
+          <button className="filter-btn">Filtreeri</button>
+        </div>
+
+        <div className="spacer" />
+      </div>
+      <Footer />
+    </div>
+  );
+}
+export default Erialad;

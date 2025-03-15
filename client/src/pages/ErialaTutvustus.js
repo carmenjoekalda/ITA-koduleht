2a859e4f16711c941d1de7038f73f2f0ed0f9da8
77f315 <77f315@gmail.com>
2025-03-15 13:22:23 +0200

    eriala materjalid box

--- client/src/pages/ErialaTutvustus.js
+++ client/src/pages/ErialaTutvustus.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import ErialaData from "../assets/ErialaData";
-
+import NavigationBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import './ErialaTutvustus.css'
 function ErialaTutvustus() {
   const { id } = useParams();
   const eriala = ErialaData[id];
@@ -10,9 +12,35 @@ function ErialaTutvustus() {
   }
   return (
     <div>
-        <h1>
-            {eriala.title}
-        </h1>
+        <NavigationBar/>
+        <div className="custom-padding">
+
+            <div style={{height: "208px"}}>
+
+            </div>
+            <h1 style={{marginBottom: "37px"}}>
+                {eriala.title}
+            </h1>
+            {/*erialapilt/vajalikud materjalid */}
+            <div className="d-flex" style={{height: "25rem",background: "#EEE", borderRadius: "10px", overflow: "hidden" ,marginBottom: "8rem"}}>
+                <div className="bg-dark" style={{width: "40%"}}>
+
+                </div>
+                <div className="flex-grow-1 d-flex flex-column align-items-center">
+                    <div className="vajalikud-materjalid-top"/>
+                    <div className=" flex-grow-1">
+                        <h1 className="mt-5">Vajalikud materjalid</h1>
+                        <button className="vajalikud-materjalid-btn mb-3">
+                            Õppekava PDF
+                        </button>
+                        <button className="vajalikud-materjalid-btn">
+                            Rakenduskava PDF
+                        </button>
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <Footer/>
     </div>
   )
 }

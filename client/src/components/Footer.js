0e99eb757f377ad0359324cbd9f6332b75d40087
77f315 <77f315@gmail.com>
2025-03-21 18:06:24 +0200

    fixed footer text color

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,21 +4,21 @@ import { Container } from "react-bootstrap";
 
 function Footer() {
   return (
-      <Container fluid className="custom-padding bg-dark text-white">
+      <Container fluid className="custom-padding bg-dark text-white" >
         <div className="d-flex align-items-center py-5 justify-content-between">
           <img src={logo} alt="Logo" className="vocologo" />
           <div className="info">
-                <h1>Kontakt</h1>
+                <h1 style={{ color: "#FBFBFB" }}>Kontakt</h1>
                 <h4>info@voco.ee</h4>
                 <h4>7 361 810</h4>
             </div>
             <div>
-                <h1>Voco</h1>
+                <h1 style={{ color: "#FBFBFB" }}>Voco</h1>
                 <h4>Kopli 1</h4>
                 <h4>Tartu 50115 Eesti</h4>
             </div>
             <div>
-                <h1>Privaatsustingimused</h1>
+                <h1 style={{ color: "#FBFBFB" }}>Privaatsustingimused</h1>
                 <h4>Andmekaitse</h4>
                 <h4>Küpsised</h4>
             </div>
